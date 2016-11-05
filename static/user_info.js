275e89e9fae4ab570709f7dd4a8ff4c846bbfc20
vagrant <vagrant@vagrant.vm>
2016-11-05 05:56:32 +0000

    Added neighborhood boundaries

--- static/user_info.js
+++ static/user_info.js
@@ -1,8 +1,22 @@
 'use strict';
-var map;
+
+
 function initMap() {
-map = new google.maps.Map(document.getElementById('map'), {
-  center: {lat: -34.397, lng: 150.644},
-  zoom: 8
-});
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 37.7749, lng: -122.4194},
+    zoom: 12
+  });
+  var src = 'http://dl.dropbox.com/s/8k8453dk6i6u33r/neighborhoods.kml';
+  var layer = new google.maps.KmlLayer(src);
+  google.maps.event.addListener(layer, 'status_changed', function() {
+    if (layer.getStatus() != 'OK') {
+      alert('Google Maps could not load. Status returned is' +layer.getStatus());
+    };
+  });
+
+
+  layer.setMap(map);
 }
+
+google.maps.event.addDomListener(window, 'load', initMap);
+
